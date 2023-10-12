6764d5a9b4d84539feab8f308bcb47e02e22ba00
Jeremias Ahumada A <jeremias.alonso77@gmail.com>
2023-10-11 21:45:41 -0400

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import TodoComputed from "./components/TodoComputed";
 import TodoCreate from "./components/TodoCreate";
 import TodoFilter from "./components/TodoFilter";
 import TodoList from "./components/TodoList";
 
-const initialStateTodos = [
-	{
-		id: 1,
-		title: "Complete online Javascript bluuweb Curse",
-		completed: true
-	},
-	{
-		id: 2,
-		title: "Go to the gym",
-		completed: false
-	},
-	{
-		id: 3,
-		title: "10 minutes meditation",
-		completed: false
-	},
-]
+const initialStateTodos = JSON.parse(localStorage.getItem("todos")) || [];
 
 const App = () => {
 
 	const [todos, setTodos] = useState(initialStateTodos);
+
+	// LocalStorage de los TODOS
+	useEffect(() => {
+		localStorage.setItem("todos", JSON.stringify(todos));
+	}, [todos]);
 
 	const createTodo = (title) => {
 		const newTodo = {
@@ -74,10 +63,15 @@ const App = () => {
 	}
 
 	return (
-		<div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain bg-gray-300 min-h-screen dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] transition-all duration-1000">
+		<div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat 
+		bg-contain bg-gray-300 min-h-screen dark:bg-gray-900 
+		dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] transition-all duration-1000
+		md:bg-[url('./assets/images/bg-desktop-light.jpg')] 
+		md:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')]"
+		>
 			<Header />
 
-			<main className="container mx-auto px-4 mt-8">
+			<main className="container mx-auto px-4 mt-8 md:max-w-xl">
 				<TodoCreate
 					createTodo={createTodo}
 				/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
     // }
 
     return (
-        <header className="container mx-auto px-4 pt-8">
+        <header className="container mx-auto px-4 pt-8 md:max-w-xl">
             <div className="flex justify-between">
                 <h1 className="uppercase text-white text-3xl font-semibold tracking-[0.3em]">Todo</h1>
                 <button onClick={() => setDarkMode(!darkMode)}>
